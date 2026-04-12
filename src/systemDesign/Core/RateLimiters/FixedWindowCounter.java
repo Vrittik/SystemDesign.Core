def7d5c5d0264aa1bb049c02beda901f85d08bd2
vrittiksharma <vrittiksharma2@gmail.com>
2026-04-13 03:34:14 +0530

    Fixed window counter improvement

--- src/systemDesign/Core/RateLimiters/FixedWindowCounter.java
+++ src/systemDesign/Core/RateLimiters/FixedWindowCounter.java
@@ -1,9 +1,9 @@
 package systemDesign.Core.RateLimiters;
 
 public class FixedWindowCounter {
-    private long windowSize;
+    private final long windowSize;
     private long windowStart;
-    private int limit;
+    private final int limit;
     private int count;
 
     public FixedWindowCounter(long windowSizeInSeconds, int limit)
@@ -17,33 +17,73 @@ public class FixedWindowCounter {
     public synchronized boolean allowRequest()
     {
         long now = System.currentTimeMillis();
+
+        // update current count to 0 if new window is encountered
         if (now - windowStart >= windowSize)
         {
-            windowStart = System.currentTimeMillis();
+            // To know if more than 1 window worth of time has moved
+            long windowsMoved = (now - windowStart) / windowSize;
+
+            // Window start only updates at lower boundary, 0s, 5s, 10s, etc...
+            windowStart += (windowSize*windowsMoved);
             count = 0;
         }
-        if (count == limit) return false;
 
-        count++;
-        return true;
+        if(count < limit)
+        {
+            count++;
+            return true;
+        }
+        return false;
     }
 
     public static void main(String[] args)
     {
         FixedWindowCounter fixedWindowCounter = new FixedWindowCounter(10, 3);
+        // Window A
         publishNRequest(fixedWindowCounter, 0, 5, 1);
 
-        // Wait for 5 seconds (for new window)
+        // Window B - Wait for 5 seconds (for new window, remaining 5 seconds in current window)
         try
         {
             System.out.println("Waiting for 5 seconds.....");
             Thread.sleep(5000);
+            publishNRequest(fixedWindowCounter, 5, 10, 1);
         }
         catch (Exception e)
         {
 
         }
-        publishNRequest(fixedWindowCounter, 5, 10, 1);
+
+        // Problem - 2x request at the edges of the windows
+        // Window C - Wait for 14 seconds (29th second - End of window C)
+        try
+        {
+            System.out.println("Waiting for 14 seconds.....");
+            Thread.sleep(14000);
+
+            // Execute 3 requests simultaneously
+            publishNRequest(fixedWindowCounter, 10, 13, 0);
+        }
+        catch (Exception e)
+        {
+
+        }
+
+        // Window D - Wait for 2 seconds (31st second - start of window D)
+        try
+        {
+            System.out.println("Waiting for 2 seconds.....");
+            Thread.sleep(2000);
+
+            // Execute 3 requests simultaneously, accepted = 6 request in 2 seconds gap
+            // which is the drawback of this. Burst at window edges
+            publishNRequest(fixedWindowCounter, 13, 16, 0);
+        }
+        catch (Exception e)
+        {
+
+        }
 
     }
 
